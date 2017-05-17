9e19c1e83138be2712fc21a74b55f071939125e9
xianfuxing <xianfuxing@126.com>
2017-05-17 14:24:39 +0800

    # 添加用户登录、注册更新ip的signal

---
+++ users/receivers.py
@@ -0,0 +1,20 @@
+from django.utils import timezone
+
+
+def update_last_login(sender, request, user, **kwargs):
+    """
+    Update last_login_ip
+    """
+    user.last_login = timezone.now()
+    user.last_login_ip = request.META.get("REMOTE_ADDR", None)
+    user.save(update_fields=['last_login', 'last_login_ip'])
+    print(user.last_login, user.last_login_ip)
+
+
+def update_joined(sender, request, user, **kwargs):
+    """
+    Update joined_ip
+    """
+    user.ip_joined = request.META.get("REMOTE_ADDR", None)
+    user.save(update_fields=['ip_joined'])
+    print(user.ip_joined)
